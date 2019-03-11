e350c94ca9db5ecc359fa16bddc72ebbdb41cdc0
essekia paul <essekiarockz@gmail.com>
2019-03-11 18:34:05 +0530

    improvements

--- app/views/single-review.php
+++ app/views/single-review.php
@@ -20,16 +20,18 @@ if (!class_exists('\HelpieReviews\App\Views\Single_Review')) {
             $this->model->stats = $post_data[0]['stats'];
         }
 
-        public function render()
+        public function get_html()
         {
-            $html = "<h1>" . $this->model->title . "</h1>";
+            $html = '';
 
-            $html .= "<p>" . $this->model->content . "</p>";
-
+            $html .= "<article>";
+            $html .= "<h1 class='title'>" . $this->model->title . "</h1>";
+            $html .= "<p class='content'>" . $this->model->content . "</p>";
             $stats_view = new \HelpieReviews\App\Views\Stats($this->model->stats);
             $html .= $stats_view->get_html();
-            $this->html = $html;
-            echo $this->html;
+            $html .= "</article>";
+
+            return $html;
         }
 
     } // END CLASS
--- test-artifacts/build/includes/templates/single-helpie_reviews.php
+++ test-artifacts/build/includes/templates/single-helpie_reviews.php
@@ -13,7 +13,7 @@ while (have_posts()): the_post();
     $wp_post = get_post();
     $review_post = new \HelpieReviews\App\Models\Review_Post($wp_post);
     $view = new \HelpieReviews\App\Views\Single_Review($review_post);
-    $view->render();
+    echo $view->get_html();
 endwhile;
 
 get_footer();
