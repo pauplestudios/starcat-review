4c851c60f7f919fda38c406e6e8b9d9f1594920b
essekia paul <essekiarockz@gmail.com>
2019-06-03 16:38:02 +0530

    Uncomment User Reviews

--- includes/hooks.php
+++ includes/hooks.php
@@ -13,7 +13,7 @@ if (!class_exists('\HelpieReviews\Includes\Hooks')) {
         {
             // error_log('hooks __construct');
             add_filter('the_content', array($this, 'content_filter'));
-            add_filter('the_excerpt', array($this, 'content_filter'));
+            // add_filter('the_excerpt', array($this, 'content_filter'));
         }
 
         public function content_filter($content)
@@ -23,7 +23,8 @@ if (!class_exists('\HelpieReviews\Includes\Hooks')) {
             $user_review_controller = new \HelpieReviews\App\Controllers\User_Reviews_Controller();
 
             $user_review_content = $user_review_controller->get_view();
-            $fullcontent = $content . $review_content . $user_review_content;
+            // $fullcontent = $content . $review_content . $user_review_content;
+            $fullcontent = $content . $review_content;
             return $fullcontent;
         }
 
