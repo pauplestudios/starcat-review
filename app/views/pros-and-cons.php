16a7d6f54d6e4150528dbf8f9f16a4f9b317a4df
essekia paul <essekiarockz@gmail.com>
2019-06-04 11:03:12 +0530

    Pros and cons

--- app/views/pros-and-cons.php
+++ app/views/pros-and-cons.php
@@ -19,8 +19,10 @@ if (!class_exists('\HelpieReviews\App\Views\ProsAndCons')) {
 
         public function get_html()
         {
-            $html = $this->get_pros_html($this->model['pros']);
+            $html = "<div class='hrv-pros-cons hrp-container '>";
+            $html .= $this->get_pros_html($this->model['pros']);
             $html .= $this->get_cons_html($this->model['cons']);
+            $html .= "</div>";
 
             $this->html = $html;
             return $this->html;
@@ -28,29 +30,35 @@ if (!class_exists('\HelpieReviews\App\Views\ProsAndCons')) {
 
         private function get_pros_html($pros)
         {
-            $html = "Pros Here";
-            $html .= '<ul>';
+
+            $html = "<div class='column'>";
+            $html .= "<h4>Pros</h4>";
+            $html .= '<ol>';
 
             for ($ii = 0; $ii < sizeof($pros); $ii++) {
                 $html .= "<li>" . $pros[$ii] . "</li>";
             }
 
             $html .= "<li>Pros here</li>";
-            $html .= "</ul>";
+            $html .= "</ol>";
+            $html .= "</div>";
 
             return $html;
         }
 
         private function get_cons_html($cons)
         {
-            $html = "Cons Here";
-            $html .= '<ul>';
+
+            $html = "<div class='column'>";
+            $html .= "<h4>Cons</h4>";
+            $html .= "<ol class='cons'>";
 
             for ($ii = 0; $ii < sizeof($cons); $ii++) {
                 $html .= "<li>" . $cons[$ii] . "</li>";
             }
 
-            $html .= "</ul>";
+            $html .= "</ol>";
+            $html .= "</div>";
 
             return $html;
         }
