78c9cf91d5723ce7e9bae1a9266c8c76fcbb8791
essekia paul <essekiarockz@gmail.com>
2019-07-26 11:19:49 +0530

    Update Upgrades Submodule

--- tests/wpunit/LibraryTest.php
+++ tests/wpunit/LibraryTest.php
@@ -9,5 +9,9 @@ class LibraryTest extends \Codeception\TestCase\WPTestCase
         require_once $library_path . 'upgrader/UpgraderTest.php';
         $upgrader_test  = new UpgraderTest();
         $upgrader_test->test_upgrader_dummy($this);
+
+        /* Test 2 */
+        $upgrader_list = new \HelpieReviews\Includes\Upgrades_List();
+        $upgrader_test->test_init($this, $upgrader_list);
     }
 } // END TEST CLASS
