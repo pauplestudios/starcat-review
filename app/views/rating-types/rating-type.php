25d059e67a43a566c8e39eec20f481e772aadaa8
essekia paul <essekiarockz@gmail.com>
2019-08-05 15:11:16 +0530

    Rating Type Class

---
+++ app/views/rating-types/rating-type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HelpieReviews\App\Views\Rating_Type;
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Rating_Type')) {
+    class Rating_Type
+    { } // END CLASS
+}
