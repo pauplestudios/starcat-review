51d24bf0c94828d6bc33b221f5373fb522a576a9
santhoshoutright <santhoshoutright@gmail.com>
2019-08-09 14:12:38 +0530

    Image rating rating ready

--- app/views/rating-types/image-rating.php
+++ app/views/rating-types/image-rating.php
@@ -15,46 +15,74 @@ if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Image_Rating')) {
         { 
            $this->props = [
                 'collection' => [
-                    'divisor' => 20,
+                    // 'divisor' => 20,
                     'show_stats' => ['overall', 'price', 'ux', 'feature', 'better', 'cool'],
                     'show_value_type' => 'number', // or percentage
                     'show_value_limit' => 10, // 20 ,30, 50, 80 and etc..   
-                    'stats_type' => 'image', // or icon or progress                    
+                    'stats_type' => 'image', // or icon or progress     
+                    'image_url' => HELPIE_REVIEWS_URL . 'includes/assets/img/tomato.png',
                     'icon' => 'circle'              
                     // 'show_user_review' => true,
                 ],
                 'items' => $stats,
             ];
-            
-        }
+            // error_log( 'Props : '. print_r($this->props, true));
+        }       
 
         public function get_html()
         {
-            $html  = '<div class=".hrp-container"><ul class="hrp-review-list">';
+            $html  = '<div class=".hrp-container">';
+            $html .= '<ul class="hrp-review-list">';
 
             foreach ($this->props['items'] as $key => $value) {
-                $html .= '<div class="single_review">';
-                $html .= '<div class="review__results__wrapper">';
-                    $html .= '<div class="review__results">';
-                    $html .= $this->get_image_icon();
-                    $html .= '</div>'; 
-                    $html .= '<span>'.$key.'</span>'; 
-                $html .= '</div></div>';
+                $html .= '<li>';
+                $html .='<div class="single_review">';
+                    $html .= '<div class="review__results__wrapper">';
+                    $html .= $this->get_image_wrapper_html();
+                    $html .= '</div>';
+                    $html .= $this->get_image_results_html(); 
+                $html .='</div>'; 
+                $html .= '<span>'.$key.'</span>'; 
+                $html .= '</li>';
             }
             $html .= '</ul></div>';
 
             return $html;
         }
 
-        public function get_image_icon()
+        public function get_image_wrapper_html()
         {
-            $html = '';            
-            $fallback_image_url = "";
-            $image_url = "$this->props['collection']['image']";
-            
+            $html = '';
+            $fallback_image_url = HELPIE_REVIEWS_URL . 'includes/assets/img/tomato.png';
+            for($ii = 0; $ii<5; $ii++){                
+                $html .= "<img src='".$fallback_image_url."'>";
+            }
 
             return $html;
         }
+
+        public function get_image_results_html()
+        {
+            $html = '';     
+            $html .= '<div class="review__results">';       
+            $fallback_image_url = HELPIE_REVIEWS_URL . 'includes/assets/img/filled-tomato.png';
+            $image_url = $this->props['collection']['image_url'];
+
+            for($ii = 0; $ii<5; $ii++){                
+                $html .= "<img src='".$fallback_image_url."'>";
+            }
+            $html .= '</div>';
+            // error_log('fallback_image_url : ' .$fallback_image_url);
+            // error_log( 'Image : '. $image_url);
+            return $html;
+        }
+
+        // protected function get_image()
+        // {
+        //     $image = get_option('helpie-reviews')['stats-list'][0]['stat_image']['url'];
+            
+        //     return $image;
+        // }
     }
 }
 
