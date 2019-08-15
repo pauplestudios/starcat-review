a017f86ddb94324ce280db51d4bec1c227b5ea4f
essekia paul <essekiarockz@gmail.com>
2019-08-15 10:26:28 +0530

    Move User Reviews Controller

--- app/widgets/user-reviews/user-reviews-controller.php
+++ app/widgets/user-reviews/user-reviews-controller.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace HelpieReviews\App\Controllers;
+namespace HelpieReviews\App\Widgets\User_Reviews;
 
 if (!defined('ABSPATH')) {
     exit;
 } // Exit if accessed directly
 
-if (!class_exists('\HelpieReviews\App\Controllers\User_Reviews_Controller')) {
+if (!class_exists('\HelpieReviews\App\Widgets\User_Reviews\User_Reviews_Controller')) {
     class User_Reviews_Controller
     {
         public function __construct()
         {
+            error_log('__construct : ' . __construct);
             $this->utils = new \HelpieReviews\Includes\Utils();
             $this->reviews = new \HelpieReviews\App\Collections\User_Reviews();
             $this->view = new \HelpieReviews\App\Views\Reviews_List();
@@ -20,7 +21,6 @@ if (!class_exists('\HelpieReviews\App\Controllers\User_Reviews_Controller')) {
         {
             return $this->view->get();
         }
-
     } // END CLASS
 
 }
