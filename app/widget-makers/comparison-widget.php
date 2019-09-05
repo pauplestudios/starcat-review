2bb001ea89cade89fb2de683115b5a2a8bf15cd7
essekia paul <essekiarockz@gmail.com>
2019-09-05 11:57:44 +0530

    Comparison Widget

---
+++ app/widget-makers/comparison-widget.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace HelpieReviews\App\Widget_Makers;
+
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+if (!class_exists('\HelpieReviews\App\Widget_Makers\Comparison_Widget')) {
+    class Comparison_Widget
+    {
+        public function get_view()
+        {
+            $comparison_controller = new \HelpieReviews\App\Widgets\Comparison\Controller();
+            $post_ids = [];
+            return $comparison_controller->get_view($post_ids);
+        }
+
+        protected function get_collection_props($args)
+        { }
+
+        protected function execute_methods_with_queries($args)
+        {
+            // $this->posts = $this->cat_posts_repo->get_category_posts($args);
+        }
+
+
+
+        public function get_default_args()
+        {
+            $args = array();
+
+            // Get Default Values from GET - FIELDS
+            $fields = $this->get_fields();
+            foreach ($fields as $key => $field) {
+                $args[$key] = $field['default'];
+            }
+
+            return $args;
+        }
+
+        public function get_fields()
+        {
+
+            $fields = array(
+                'title' => [
+                    'name' => 'title',
+                    'label' => __('Title', 'helpie-faq'),
+                    'default' => 'Title of Comparison Table',
+                    'type' => 'text',
+                ],
+                'max_num_of_items' => [
+                    'name' => 'num_of_cols',
+                    'label' => __('Maximum Number of Items', 'helpie-faq'),
+                    'default' => 'two',
+                    'options' => array(
+                        '1' => __('1', 'helpie-faq'),
+                        '2' => __('2', 'helpie-faq'),
+                        '3' => __('3', 'helpie-faq'),
+                    ),
+                    'type' => 'select',
+                ],
+            );
+
+
+            return $fields;
+        }
+
+        public function get_style_config()
+        { }
+    } // END CLASS
+}
