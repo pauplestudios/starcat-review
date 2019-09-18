e1b5e74333ac1e60faa57588c161311dcc923f36
Santhosh <santhoshoutright@gmail.com>
2019-09-18 12:26:38 +0530

    Star stats html structure changed to more readable.

--- app/views/rating-types/star-rating.php
+++ app/views/rating-types/star-rating.php
@@ -11,20 +11,19 @@ if (!defined('ABSPATH')) {
 if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Star_Rating')) {
     class Star_Rating extends Rating_Type
     {
-        private $html;
-
         public function __construct($viewProps)
         {
             $this->props = $viewProps;
         }
 
-        public function get_html()
+        public function get_view()
         {
             $stats_cumulative_score = 0;
             $count = 0;
 
-            $html = '<div class="hrp-container">';
-            $html .= '<ul class="hrp-review-list">';
+            $html = '<ul class="reviewed-list"
+                data-animate="' . $this->props['collection']['animate'] . '"
+            >';
             $stat_html = '';
 
             foreach ($this->props['items'] as $key => $value) {
@@ -33,7 +32,7 @@ if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Star_Rating')) {
                 $score = $this->get_score($value);
 
                 if ($this->is_stat_included($key, $this->props['collection'])) {
-                    $stat_html .= $this->get_single_stat($key, $value, $score);
+                    $stat_html .= $this->get_reviewed_stat($key, $value, $score);
                 }
 
                 $count++;
@@ -42,23 +41,52 @@ if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Star_Rating')) {
             $overall_stat_html = $this->get_overall_stat_html($stats_cumulative_score, $count);
 
             $html .= $overall_stat_html . $stat_html;
-
-            $html .= '</ul></div>';
+            $html .= '</ul>';
 
             return $html;
         }
 
-        public function get_single_stat($key, $value, $score)
+        public function get_review_stat($key, $value, $score)
         {
-            $html = '';
-            $html .= '<li>';
-            $html .= '<div class="single-review">';
-            $html .= '<div class="single-review__wrapper">';
+            $html = '<li class="review-item">';
+
+            $html .= '<div class="review-item-stars"
+                title="' . $score . ' / ' . $this->props['collection']['star_scale'] . '"
+            >';
             $html .= $this->get_wrapper_html();
+            $html .= $this->get_result_html();
+            $html .= '<input type="hidden" name="score" value="' . $value . '">';
             $html .= '</div>';
-            $html .= $this->get_results_html($key, $value, $score);
+
+            $html .= '<div class="review-item-label">';
+            $html .= '<span class="reviewitem-label__text">' . $key . '</span>';
+            $html .= '<span class="review-item-label__divider"> - </span>';
+            $html .= '<span class="review-item-label__score">' . $score . '</span>';
             $html .= '</div>';
-            $html .= '<div class="single-review__label">' . $key . ' - <span>' . $score . '</span></div>';
+
+
+            $html .= '</li>';
+
+            return $html;
+        }
+
+        protected function get_reviewed_stat($key, $value, $score)
+        {
+            $html = '<li class="reviewed-item">';
+
+            $html .= '<div class="review-item-stars"
+                title="' . $score . ' / ' . $this->props['collection']['star_scale'] . '"
+            >';
+            $html .= $this->get_wrapper_html();
+            $html .= $this->get_result_html();
+            $html .= '<input type="hidden" name="score" value="' . $value . '">';
+            $html .= '</div>';
+
+            $html .= '<div class="reviewed-item-label">';
+            $html .= '<span class="reviewed-item-label__text">' . $key . '</span>';
+            $html .= '<span class="reviewed-item-label__divider"> - </span>';
+            $html .= '<span class="reviewed-item-label__score">' . $score . '</span>';
+            $html .= '</div>';
 
             $html .= '</li>';
 
@@ -71,37 +99,29 @@ if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Star_Rating')) {
             $overall_value = $stats_cumulative_score / $count;
             $overall_score = $this->get_score($overall_value);
 
-            $overall_stat_html = $this->get_single_stat(__('Overall', 'helpie-reviews'), $overall_value, $overall_score);
+            $overall_stat_html = $this->get_reviewed_stat(__('Overall', 'helpie-reviews'), $overall_value, $overall_score);
 
             return $overall_stat_html;
         }
 
-        protected function get_score($value)
-        {
-            $divisor = ($this->props['collection']['star_scale'] == 10) ? 10 : 20;
-            $score = $value / $divisor;
-
-            return (floor($score * 2) / 2);
-        }
-
         protected function get_wrapper_html()
         {
-            $html = '';
-
             $outline_icon = $this->props['collection']['outline_icon'];
             $icon_html = "<i class='" . $outline_icon . "'></i>";
             $image_html = "<img src='" . $outline_icon . "' />";
 
             $outline_icon_html = ($this->props['collection']['source_type'] == 'icon') ? $icon_html : $image_html;
 
+            $html = "<div class='stars-wrapper'>";
             for ($ii = 0; $ii < $this->props['collection']['star_scale']; $ii++) {
                 $html .= $outline_icon_html;
             }
+            $html .= "</div>";
 
             return $html;
         }
 
-        protected function get_results_html($key, $value, $score)
+        protected function get_result_html()
         {
             $icon = $this->props['collection']['icon'];
             $filled_icon_html = "<i class='" . $icon . "'></i>";
@@ -109,25 +129,21 @@ if (!class_exists('\HelpieReviews\App\Views\Rating_Types\Star_Rating')) {
 
             $icon_html = ($this->props['collection']['source_type'] == 'icon') ? $filled_icon_html : $filled_image_html;
 
-            $html = '';
-            $html .= '<div
-                class="single-review__results"
-                name="' . $key . '"
-                title="' . $score . ' / ' . $this->props['collection']['star_scale'] . '"
-                value="' . $value . '"
-                data-rating="0"
-                data-animate="' . $this->props['collection']['animate'] . '"
-                style="width: 0%"
-                >';
-
-
+            $html = '<div class="stars-result" style="width: 0%">';
             for ($ii = 0; $ii < $this->props['collection']['star_scale']; $ii++) {
                 $html .= $icon_html;
             }
-
             $html .= '</div>';
 
             return $html;
+        }
+
+        private function get_score($value)
+        {
+            $divisor = ($this->props['collection']['star_scale'] == 10) ? 10 : 20;
+            $score = $value / $divisor;
+
+            return (floor($score * 2) / 2);
         }
     }
 }
--- app/widgets/stats/controller.php
+++ app/widgets/stats/controller.php
@@ -11,35 +11,34 @@ if (!class_exists('\HelpieReviews\App\Widgets\Stats\Controller')) {
     {
         public function __construct($post_id)
         {
-            $this->model = new \HelpieReviews\App\Widgets\Stats\Model($post_id);                        
+            $this->model = new \HelpieReviews\App\Widgets\Stats\Model($post_id);
         }
 
         public function get_view()
-        {            
+        {
             $viewProps = $this->model->get_viewProps();
             return $this->get_stat_set($viewProps);
-        }        
+        }
 
         protected function get_stat_set($viewProps)
-        {   
+        {
             $display_rating_type = $viewProps['collection']['display_rating_type'];
 
             switch ($display_rating_type) {
-                case "star":                    
+                case "star":
                     $this->star_rating = new \HelpieReviews\App\Views\Rating_Types\Star_Rating($viewProps);
-                    return $this->star_rating->get_html();                   
+                    return $this->star_rating->get_view();
                 case "bar":
                     $this->bar_rating = new \HelpieReviews\App\Views\Rating_Types\Bar_Rating($viewProps);
-                    return $this->bar_rating->get_html();                   
+                    return $this->bar_rating->get_html();
                 case "circle":
                     $this->circle_rating = new \HelpieReviews\App\Views\Rating_Types\Circle_Rating($viewProps);
-                    return $this->circle_rating->get_html();                    
+                    return $this->circle_rating->get_html();
                 default:
                     $this->star_rating = new \HelpieReviews\App\Views\Rating_Types\Star_Rating($viewProps);
-                    return $this->star_rating->get_html();                
+                    return $this->star_rating->get_view();
             }
         }
-        
     } // END CLASS
 
 }
