b8fd1b00775f61c21c4717a23c95606bc39669b5
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2019-09-21 01:31:13 +0530

    make some changes in search field events

--- includes/assets/js/components/product-compare/search.js
+++ includes/assets/js/components/product-compare/search.js
@@ -1,18 +1,23 @@
+var productsTable = require("../../comparison-table.js");
+
 var Search = {
   init: function() {
     this.bindEvents();
   },
   bindEvents: function() {
-    var search_element = jQuery(".hrp-search-container");
-    this.searchProduct(search_element);
+    var search_element_container = jQuery(".hrp-search-container");
+    this.searchProduct(search_element_container);
+    this.selectItem(search_element_container);
+    this.closeSearchListDiv();
+    var props = {};
+    this.addItemInCompareTable(props);
   },
   searchProduct: function(search_element) {
     console.log(this);
     var search_list = jQuery(search_element).find(".hrp-search-lists");
     jQuery(search_element).on("keyup", ".hrp-search", function() {
       var search_data = {
-        action: "get_hrp_results",
-        search_key: jQuery(this).val()
+        action: "get_hrp_results"
       };
 
       jQuery.ajax({
@@ -32,21 +37,70 @@ var Search = {
             for (let i = 0; i < resultDatalength; i++) {
               console.log(result_data[i]);
               var content = "";
-              content += '<div class="items">' + result_data[i] + "</div>";
+              content += '<div class="item">' + result_data[i] + "</div>";
               jQuery(search_list).append(content);
             }
           } else {
             var content = "";
-            content += '<div class="items">Not found data</div>';
+            content += '<div class="item">Not found data</div>';
             jQuery(search_list).append(content);
           }
         }
       });
     });
+  },
+  selectItem: function(search_element_container) {
+    jQuery(search_element_container).on(
+      "click",
+      ".hrp-search-lists .item",
+      function(e) {
+        e.preventDefault();
+        let heading = jQuery(this)
+          .text()
+          .trim();
+        jQuery(search_element_container)
+          .find(".hrp-search")
+          .val(heading);
+        jQuery(".hrp-search-lists").hide();
+
+        contents = {
+          headingText: heading
+        };
+        new Search.addItemInCompareTable(contents);
+      }
+    );
+  },
+  closeSearchListDiv: function() {
+    jQuery(document).mouseup(function(e) {
+      container = jQuery(".hrp-search-lists");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+      }
+    });
+  },
+  addItemInCompareTable: function(options) {
+    if (Object.keys(options).length === 0) {
+      return 1;
+    } else {
+      let content = "";
+      content = '<li class="product"><div class="top-info">';
+      content += '<div class="check"></div>';
+      content += '<img src="" alt="" class="featured-image">';
+      content += "<h3>" + options.headingText + "</h3></div>";
+      content +=
+        '<ul class="cd-features-list"><li>5</li><li>4.5</li><li>4.5</li></ul>';
+      content += "</div></li>";
+
+      jQuery(".cd-products-wrapper")
+        .find(".cd-products-columns")
+        .append(content);
+
+      // jQuery(".cd-products-comparison-table").each(function() {
+      //   //create a productsTable object for each .cd-products-comparison-table
+      //   new productsTable.productsTable(jQuery(this));
+      // });
+    }
   }
-  // clearSearchListContainer: function(hrp_search_list_container) {
-  //   jQuery(hrp_search_list_container).empty();
-  // }
 };
 
 module.exports = Search;
