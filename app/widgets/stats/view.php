43a15f343d387c165022d514d90459b1e350390a
Santhosh <santhoshoutright@gmail.com>
2019-09-30 17:26:18 +0530

    get_rating_stat_set mehtod changed to get view.

--- app/widgets/stats/view.php
+++ app/widgets/stats/view.php
@@ -16,34 +16,9 @@ if (!class_exists('\HelpieReviews\App\Widgets\Stats\View')) {
 
         public function get()
         {
-            $post_id = $this->viewProps['collection']['post_id'];
-            $this->star_rating = new \HelpieReviews\App\Views\Rating_Types\Star_Rating($this->viewProps);
-            $this->bar_rating = new \HelpieReviews\App\Views\Rating_Types\Bar_Rating($this->viewProps);
-            $this->prosandcons = new \HelpieReviews\App\Widgets\ProsAndCons\Controller($post_id);
+            $viewProps = $this->viewProps;
 
-            $html = '<div class="ui stackable two column grid">';
-            $html .= '<div class="column">';
-            // Author Stat
-            $html .= $this->star_rating->get_view();
-            $html .= $this->prosandcons->get_view();
-            $html .= '</div>';
-
-            $html .= '<div class="column">';
-            // User Stat
-            $html .= $this->bar_rating->get_view();
-            $html .= $this->prosandcons->get_view();
-            $html .= '</div>';
-            $html .= '</div>';
-
-            return $html;
-        }
-
-
-        protected function get_rating_stat_set($viewProps)
-        {
-            $display_rating_type = $viewProps['collection']['type'];
-
-            switch ($display_rating_type) {
+            switch ($viewProps['collection']['type']) {
                 case "star":
                     $this->star_rating = new \HelpieReviews\App\Views\Rating_Types\Star_Rating($viewProps);
                     return $this->star_rating->get_view();
