28c014c31275fbc8a58e47da561169f7651783c2
Santhosh <santhoshoutright@gmail.com>
2019-10-02 11:29:46 +0530

    Single post merge fix to load user review form

--- includes/templates/single/view.php
+++ includes/templates/single/view.php
@@ -16,12 +16,14 @@ if (!class_exists('\HelpieReviews\Includes\Templates\Single\View')) {
 
         public function get_html()
         {
-            $summary = new \HelpieReviews\App\Widgets\Summary\Controller();
+            $summary = new \HelpieReviews\App\Components\Summary\Controller();
+            $form_controller = new \HelpieReviews\App\Components\Form\Controller($this->post->ID);
 
             $html = "<article>";
             $html .= "<h1 class='title'>" . $this->post->post_title . "</h1>";
             $html .= "<p class='content'>" . $this->post->post_content . "</p>";
             $html .= $summary->get_view($this->post->ID);
+            $html .= $form_controller->get_view();
             $html .= "</article>";
 
             return $html;
