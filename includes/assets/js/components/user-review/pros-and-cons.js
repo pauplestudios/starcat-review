f02006dd57630c30bfbb48f5f5f2b63edcf3c083
Santhosh <santhoshoutright@gmail.com>
2019-10-09 18:51:30 +0530

    Refactoring ProsandCons Script

--- includes/assets/js/components/user-review/pros-and-cons.js
+++ includes/assets/js/components/user-review/pros-and-cons.js
@@ -13,6 +13,16 @@ var ProsAndCons = {
     getRepeater: function(selector, group) {
         var list = jQuery(selector).find("[data-repeater-list=" + group + "]");
 
+        ProsAndCons.addItem(selector, list, group);
+        ProsAndCons.deleteItem(selector, list, group);
+
+        // Already loaded element Pros and Cons Dropdown
+        jQuery(selector + " .ui.dropdown").dropdown({
+            allowAdditions: true
+        });
+    },
+
+    addItem: function(selector, list, group) {
         const duplicateItem = list
             .find("[data-repeater-item]")
             .first()
@@ -20,20 +30,39 @@ var ProsAndCons = {
             .html();
 
         jQuery(selector + " [data-repeater-create]").on("click", function() {
-            let indexedItem = ProsAndCons.setIndexToField(
-                list,
-                duplicateItem,
-                group
-            );
+            let indexedItem = ProsAndCons.setIndex(list, duplicateItem, group);
 
             list.append(indexedItem);
-            ProsAndCons.reiniateEvents(selector, list, group);
-        });
+            ProsAndCons.deleteItem(selector, list, group);
+            ProsAndCons.reinitiateEvents(list, group);
 
-        ProsAndCons.reiniateEvents(selector, list, group);
+            // Pros and Cons Dropdown Items
+            jQuery(selector + " .ui.dropdown").dropdown({
+                allowAdditions: true
+            });
+        });
     },
 
-    setIndexToField: function(list, item, dataAttr) {
+    deleteItem: function(selector, list, group) {
+        jQuery(selector + " [data-repeater-item] [data-repeater-delete]").on(
+            "click",
+            function() {
+                jQuery(this)
+                    .parent()
+                    .parent()
+                    .remove();
+                ProsAndCons.updateIndex(list, group);
+                ProsAndCons.reinitiateEvents(list, group);
+            }
+        );
+    },
+
+    reinitiateEvents: function(list, group) {
+        ProsAndCons.setRules(list, group);
+        Form.formValidation(formFields);
+    },
+
+    setIndex: function(list, item, dataAttr) {
         let key = list.children().length;
 
         let field = jQuery(item);
@@ -54,46 +83,7 @@ var ProsAndCons = {
         return indexedHtml;
     },
 
-    reiniateEvents: function(selector, list, group) {
-        ProsAndCons.getItemDelete(
-            selector + " [data-repeater-item] [data-repeater-delete]",
-            list,
-            group
-        );
-
-        ProsAndCons.updateValidateRules(list, group);
-        Form.formValidation(formFields);
-        jQuery(selector + " .ui.dropdown").dropdown({
-            allowAdditions: true
-        });
-    },
-
-    getItemDelete: function(selector, list, group) {
-        jQuery(selector).on("click", function() {
-            jQuery(this)
-                .parent()
-                .parent()
-                .remove();
-            ProsAndCons.updateFieldsIndex(list, group);
-            ProsAndCons.updateValidateRules(list, group);
-            ProsAndCons.reiniateEvents(selector, list, group);
-        });
-    },
-
-    updateValidateRules: function(list, group) {
-        let items = list.find("[data-repeater-item]");
-        let count = 0;
-        items.each(function(index, item) {
-            let field = jQuery(item)
-                .find("[data-" + group + "]")
-                .attr("name");
-
-            formFields[field] = ProsAndCons.generateField(field, group);
-            count++;
-        });
-    },
-
-    updateFieldsIndex: function(list, group) {
+    updateIndex: function(list, group) {
         let items = list.find("[data-repeater-item]");
 
         let count = 0;
@@ -105,7 +95,17 @@ var ProsAndCons = {
         });
     },
 
-    generateField: function(identifier, group) {
+    setRules: function(list, group) {
+        let items = list.find("[data-repeater-item]");
+        items.each(function(index, item) {
+            let field = jQuery(item)
+                .find("[data-" + group + "]")
+                .attr("name");
+            formFields[field] = ProsAndCons.updateRules(field, group);
+        });
+    },
+
+    updateRules: function(identifier, group) {
         return {
             identifier: identifier,
             rules: [
