86ad1b713c4ec933dd54ce9dedc9db4a1d99bb77
Santhosh <santhoshoutright@gmail.com>
2019-10-11 11:19:45 +0530

    Compare table commented out from Widget, Style, Script and Category Template Hook

--- includes/assets/js/main.js
+++ includes/assets/js/main.js
@@ -1,7 +1,7 @@
 var UserReview = require("./components/user-review.js");
 var ListControl = require("./blocks/list-control.js");
 var ReviewsList = require("./components/reviews-list.js");
-var ComparisonTable = require("./comparison-table.js");
+// var ComparisonTable = require("./comparison-table.js");
 var UserReviewsList = require("./components/user-reviews-list.js");
 
 var HelpieReviews = {
