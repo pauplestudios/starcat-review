4ba6f03faeaad6f1a6fc44c9c9242a1b265a4bcc
essekia paul <essekiarockz@gmail.com>
2020-01-24 11:07:15 +0530

    Basic Addon Testing

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -29,4 +29,37 @@ class BasicCest
         $I->amOnPluginsPage();
         $I->seePluginActivated('starcat-review');
     }
+
+    public function cptAddonActivation(\AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+
+        $I->amOnPluginsPage();
+        $I->activatePlugin('starcat-review-cpt-addon');
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginActivated('starcat-review-cpt-addon');
+
+        /* Settings Page */
+        $I->amOnPage('/wp-admin/admin.php?page=scr-settings#tab=1');
+        $I->see('Main Page');
+        $I->see('Category Page');
+        $I->see('Single Page');
+    }
+
+    public function comparisonTableAddonActivation(\AcceptanceTester $I)
+    {
+        $data_slug = 'starcat-review-comparison-table-addon';
+        $I->loginAsAdmin();
+
+        $I->amOnPluginsPage();
+        $I->activatePlugin($data_slug);
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginActivated($data_slug);
+
+        /* Settings Page */
+        $I->amOnPage('/wp-admin/admin.php?page=scr-settings#tab=1');
+        $I->see('Where to include reviews?');
+    }
 }
