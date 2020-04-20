7fa9ca730b23d8927e59bed664cf59ab2cbc4fe0
Santhosh <santhoshoutright@gmail.com>
2020-04-20 11:52:15 +0530

    renamed apply_filter get_single_review_photos_field to get_single_review_upload_photos_field

--- app/components/form/view.php
+++ app/components/form/view.php
@@ -79,7 +79,7 @@ if (!class_exists('\StarcatReview\App\Components\Form\View')) {
                 $html .= $this->get_pros_and_cons();
             }
 
-            $html .= apply_filters('scr_photo_reviews/get_single_review_photos_field', $review);
+            $html .= apply_filters('scr_photo_reviews/get_single_review_upload_photos_field', $review);
 
             if ($this->props['collection']['show_captcha']) {
                 $html .= Recaptcha::load_v2_html();
