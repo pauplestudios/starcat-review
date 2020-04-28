9e5246c8846ba36b94015b64e19385c342c5ea11
essekia <essekiarockz@gmail.com>
2020-04-28 11:37:29 +0530

    changed config to bin

--- bin/setup-pipelines.sh
+++ bin/setup-pipelines.sh
@@ -1,4 +1,4 @@
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up -d
-./bin/config/wait-for-it.sh http://localhost:81
+./bin/wait-for-it.sh http://localhost:81
