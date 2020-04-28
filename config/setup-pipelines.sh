08e648a24f05bd95cc7e7a4d563b5feed4b0a177
essekia <essekiarockz@gmail.com>
2020-04-28 11:28:57 +0530

    Addition Step 1 of Pipelines

---
+++ config/setup-pipelines.sh
@@ -0,0 +1,4 @@
+curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose -f docker-compose.yml up -d
+./bin/config/wait-for-it.sh http://localhost:81
