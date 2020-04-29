f351561daaa88f1187951523def283e7aeae9519
essekia <essekiarockz@gmail.com>
2020-04-29 11:52:05 +0530

    update docker-compose versions

--- bin/setup-pipeline.sh
+++ bin/setup-pipeline.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up -d
 chmod +x ./bin/wait-for-it.sh
