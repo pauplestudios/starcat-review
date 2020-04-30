587b5fa6fec532a5ce272315bf93fe6d57e06a37
essekia <essekiarockz@gmail.com>
2020-04-30 16:09:51 +0530

    setup addons before docker-compose

--- bin/setup-pipeline.sh
+++ bin/setup-pipeline.sh
@@ -1,3 +1,5 @@
+cp -r vendor/pauple/starcat-review-cpt artifacts/addons
+cp -r vendor/pauple/starcat-review-ct artifacts/addons
 curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up -d
