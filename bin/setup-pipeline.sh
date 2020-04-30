660eb2b6cc4ee488e4d44b2a06bc1fa4082debec
essekia <essekiarockz@gmail.com>
2020-04-30 16:15:06 +0530

    ls addons

--- bin/setup-pipeline.sh
+++ bin/setup-pipeline.sh
@@ -1,5 +1,7 @@
 cp -r vendor/pauple/starcat-review-cpt artifacts/addons
 cp -r vendor/pauple/starcat-review-ct artifacts/addons
+ls artifacts/addons/starcat-review-cpt
+ls artifacts/addons/starcat-review-ct
 curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up -d
