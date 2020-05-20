3e709f1f32696bde0c07eae1e95464b29342f945
essekia <essekiarockz@gmail.com>
2020-05-20 16:08:23 +0530

    wait for port 80

--- bin/setup-pipeline.sh
+++ bin/setup-pipeline.sh
@@ -2,5 +2,5 @@ curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compos
 chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up -d
 chmod +x ./bin/wait-for-it.sh
-./bin/wait-for-it.sh http://localhost:81
+./bin/wait-for-it.sh http://localhost:80
 sleep 20
