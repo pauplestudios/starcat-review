e6dad4cd1c9ce2eb77dd6449523a3b551a5713ad
essekia <essekiarockz@gmail.com>
2020-05-21 13:17:48 +0530

    upgrader module via composer + test fix

---
+++ bin/addons.sh
@@ -0,0 +1,10 @@
+cd vendor/pauple/starcat-review-cpt && yarn copyfiles
+cd dist && cpy * ../../../../artifacts/addons/starcat-review-cpt --parents
+bestzip ../../../../starcat-review-cpt.zip *
+cd ../../../../
+cd vendor/pauple/starcat-review-ct && yarn copyfiles
+cd dist && cpy * ../../../../artifacts/addons/starcat-review-ct --parents
+bestzip ../../../../starcat-review-ct.zip *
+cd ../../../../
+
+cpy vendor/pauple/upgrader/*.php includes/lib/upgrader
