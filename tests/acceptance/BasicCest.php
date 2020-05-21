a00fd4dd7097c839591c9e59615e293f6408ce44
essekia <essekiarockz@gmail.com>
2020-05-21 12:16:11 +0530

    enabled cpt for test

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -101,5 +101,9 @@ class BasicCest
         /* Settings Page */
         $I->amOnPage('/wp-admin/admin.php?page=scr-settings#tab=1');
         $I->see('Where to include reviews?');
+
+        /* Check Available Menus */
+        $I->see('General Settings');
+        $I->see('User Review');
     }
 }
