4c4269b6cc21bdf861528e0b5086a75f837a38e1
essekia <essekiarockz@gmail.com>
2020-05-21 14:32:07 +0530

    fix starcat-review-woo-notify for pipeline

--- bin/addons.sh
+++ bin/addons.sh
@@ -6,5 +6,7 @@ cd vendor/pauple/starcat-review-ct && yarn copyfiles
 cd dist && cpy * ../../../../artifacts/addons/starcat-review-ct --parents
 bestzip ../../../../starcat-review-ct.zip *
 cd ../../../../
-
+cd vendor/pauple/starcat-review-woo-notify && yarn copyfiles
+cd dist && cpy * ../../../../includes/lib/starcat-review-woo-notify --parents
+bestzip ../../../../starcat-review-cpt.zip *
 cpy vendor/pauple/upgrader/*.php includes/lib/upgrader
