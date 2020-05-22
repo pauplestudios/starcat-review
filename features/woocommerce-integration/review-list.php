e6c56b0cd8efeb7c9207ab77f5fcc3949ce280ac
Santhosh <santhoshoutright@gmail.com>
2020-05-22 18:42:31 +0530

    Listing stored title and prosandcons

--- features/woocommerce-integration/review-list.php
+++ features/woocommerce-integration/review-list.php
@@ -11,12 +11,24 @@ if (!class_exists('\StarcatReview\Features\Woocommerce_Integration\Review_List')
     {
         public function __construct()
         {
-            error_log('!!! List !!!');
+            // error_log('!!! List !!!');
+            add_action('woocommerce_review_before_comment_meta', [$this, 'list_result']);
         }
 
-        public function list_result()
+        public function list_result($comment)
         {
-            // show resulted List
+            $review = get_comment_meta($comment->comment_ID, "scr_user_review_props", true);
+            $args = [
+                'items' => $review,
+            ];
+            $html = '';
+            if ($review) {
+                $html .= '<strong class="review-title">' . $review['title'] . '</strong>';
+                $prosandcons = new \StarcatReview\App\Components\ProsAndCons\Controller();
+                $html .= $prosandcons->get_view($args);
+            }
+
+            echo $html;
         }
     }
 }
