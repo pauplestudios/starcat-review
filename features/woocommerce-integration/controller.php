b8cdb5ff269c14ef3ed6dc55623340240eb1dad3
Santhosh <santhoshoutright@gmail.com>
2020-05-23 12:00:49 +0530

    integrated temprory product listing

--- features/woocommerce-integration/controller.php
+++ features/woocommerce-integration/controller.php
@@ -11,20 +11,20 @@ if (!class_exists('\StarcatReview\Features\Woocommerce_Integration\Controller'))
     {
         public function __construct()
         {
-            error_log('!!! Woocommerce Integration Controller !!!');
+            // error_log('!!! Woocommerce Integration Controller !!!');
         }
 
         public function load()
         {
-            error_log('!!! Loading Core Woo Hooks !!!');
+            // error_log('!!! Loading Core Woo Hooks !!!');
 
-            $review_form = new \StarcatReview\Features\Woocommerce_Integration\Review();
+            // $review_form = new \StarcatReview\Features\Woocommerce_Integration\Review();
 
-            $summary = new \StarcatReview\Features\Woocommerce_Integration\Summary();
+            // $summary = new \StarcatReview\Features\Woocommerce_Integration\Summary();
 
             $product_review_list = new \StarcatReview\Features\Woocommerce_Integration\Review_List();
 
-            $migration = new \StarcatReview\Features\Woocommerce_Integration\Migration();
+            // $migration = new \StarcatReview\Features\Woocommerce_Integration\Migration();
         }
 
     }
