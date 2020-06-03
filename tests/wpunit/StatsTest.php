cffa5865753d44f08362734d5af3db176ffa7f8b
Santhosh <santhoshoutright@gmail.com>
2020-06-03 18:23:35 +0530

    defined expected and proposed in statstest

--- tests/wpunit/StatsTest.php
+++ tests/wpunit/StatsTest.php
@@ -12,16 +12,38 @@ class StatsTest extends \Codeception\TestCase\WPTestCase
 
     public function test_stat()
     {
+        /* Single Review */
+        // mulitple
         $expected = [
             'stats' => [
                 'stat_key_1' => 'value', // number
                 'stat_key_2' => 'value', // number
                 'stat_key_3' => 'value', // number
             ],
-            'stat' => 'value', // number
+            'aggregate' => 'value', // number
         ];
 
-        $data = [
+        // single
+        $expected = [
+            'stat' => [
+                'stat_key_1' => 'value', // number
+            ],
+        ];
+
+        /* summary review */
+        // multiple users
+        $expected = [
+            'stats' => [
+                'stat_key_1' => 'value', // number
+                'stat_key_2' => 'value', // number
+                'stat_key_3' => 'value', // number
+            ],
+            'aggregate' => 'value', // number
+        ];
+
+        // 100 percentage
+
+        $improved_data = [
             0 => [
                 'speed' => 100,
                 'quality' => 50,
@@ -39,12 +61,29 @@ class StatsTest extends \Codeception\TestCase\WPTestCase
             ],
         ];
 
+        $existing_data = [
+            'stats' => [
+                'feature' => [
+                    'stat_name' => 'feature',
+                    'rating' => 100,
+                ],
+                'speed' => [
+                    'stat_name' => 'speed',
+                    'rating' => 80,
+                ],
+            ],
+        ];
+
         // Settings available stat
         $global_stats = ['feature', 'speed', 'ui', 'design']; // 400 Current
 
         // Author and User -- Post level
         // Author -- Post Level
         // User reviews --  review Level
+
+        $stats_model = new \StarcatReview\App\Components\Stats\Model();
+        $model_props = $stats_model->get_itemsProps([]);
+        error_log('model_props : ' . print_r($model_props, true));
     }
 
 /*
