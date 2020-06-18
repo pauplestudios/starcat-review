cdf484ef6866021545a8f720919a22c2bbda134a
Santhosh <santhoshoutright@gmail.com>
2020-06-18 14:12:46 +0530

    get_comment_args now robusts

--- app/services/comments-factory.php
+++ app/services/comments-factory.php
@@ -15,51 +15,46 @@ if (!class_exists('\StarcatReview\App\Services\Comments_Factory')) {
 
         public function get_comments_args($post_id, $use_cases = ['stats'])
         {
-            $components = ['stats' => []];
+            $identical = 'stats';
+            $comments = [$identical => []];
             $comment_ids = $this->get_comments_ids($post_id);
-
+            
             if ($this->is_set($comment_ids)) {
                 foreach ($comment_ids as $comment_id) {
-
+                    
                     $review = get_comment_meta($comment_id, SCR_COMMENT_META, true);
-                    $stat = $this->get_stat($post_id, $comment_id, $review);
-                    $vote = $this->get_vote($review);
-                    $comment = $this->get_comment($comment_id, $review);
-                    $proandcon = $this->get_proandcon($review);
-                    $attachment = $this->get_attachment($review);
 
-                    if ($this->is_set($stat)) {
-                        $components['stats'][$comment_id] = $stat;
-                    }
-                    if ($this->is_set($proandcon)) {
-                        $components['prosandcons'][$comment_id] = $proandcon;
-                    }
-                    if ($this->is_set($vote)) {
-                        $components['votes'][$comment_id] = $vote;
-                    }
-                    if ($this->is_set($vote)) {
-                        $components['attachments'][$comment_id] = $attachment;
-                    }
-                    if ($this->is_set($comment)) {
-                        $components['comments'][$comment_id] = $comment;
-                    }
+                    foreach ($use_cases as $case) {
+                        $identical = $case;
+                        if ($case == 'stats') {
+                            $comments[$case][$comment_id] = $this->get_stat($post_id, $comment_id, $review);
+                        }
 
+                        if ($case == 'prosandcons') {
+                            $comments[$case][$comment_id] = $this->get_proandcon($review);
+                        }
+                        
+                        if ($case == 'votes') {
+                            $comments[$case][$comment_id] = $this->get_vote($review);
+                        }
+
+                        if ($case == 'attachments'){
+                            $comments[$case][$comment_id] = $this->get_attachment($review);
+                        }                       
+                        
+                        if ($case == 'comments') {
+                            $comments[$case][$comment_id] = $this->get_comment($comment_id, $review);
+                        }
+                    }                        
                 }
 
             }
 
-            if ($this->is_set($use_cases) && count($use_cases) > 1) {
-                $cases = [];
-                foreach ($use_cases as $case) {
-                    $is_exist = array_key_exists($case, $components);
-                    if ($is_exist) {
-                        $cases[$case] = $components[$case];
-                    }
-                }
-                return $cases;
+            if(count($use_cases) == 1 && array_key_exists($identical, $comments)){
+                return $comments[$identical];
             }
 
-            return $components['stats'];
+            return $comments;
 
         }
 
