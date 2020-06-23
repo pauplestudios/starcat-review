a7004080e5bed6bcbbb78d66f04ad8ec8d028f9e
Santhosh <santhoshoutright@gmail.com>
2020-06-23 15:22:15 +0530

    fix on editing non stat form

--- includes/assets/js/components/user-review/edit.js
+++ includes/assets/js/components/user-review/edit.js
@@ -42,14 +42,11 @@ var Edit = {
             reviewContent.hide();
 
             // Forms attributes and show the form
-
             var form = jQuery(selectors.reviewForm);
-            // var commentID = reviewContent.closest(".comment").attr("id");
             var commentProps = reviewContent.closest(".comment").data("props");
-            // console.log("title" + reviewContent.text().trim());
+
             form.attr("data-comment-id", reviewContent.closest(".comment").attr("id"));
-            form = Edit.modify_form_for_editing(form, commentProps, reviewContent);
-            // form.show();
+            form = Edit.modifyFormForEditing(form, commentProps, reviewContent);
 
             // Append clonned edit form into closest review content of clicked edit link
             reviewContent.after(form).next(selectors.reviewForm);
@@ -62,7 +59,7 @@ var Edit = {
         });
     },
 
-    modify_form_for_editing: function (form, props, content) {
+    modifyFormForEditing: function (form, props, content) {
         var sumbitBtn = '<div class="ui blue submit mini button">Save</div>';
         var cancelBtn = '<div class="ui cancel mini button">Cancel</div>';
 
@@ -70,6 +67,7 @@ var Edit = {
         props.description = content.find('.description').text().trim();
 
         form.addClass('mini');
+        form.attr("data-method", "PUT");
         form.find('h2.ui.header').remove();
         form.find("h5").addClass("ui tiny header");
         form.find('[name="title"]').val(props.title);
@@ -77,30 +75,41 @@ var Edit = {
         form.find(".submit.button").closest('.field').html(sumbitBtn + cancelBtn);
         form.show();
 
-        // Stats Items and Values
-        form.find('.review-list li').each(function () {
-            var item = jQuery(this);
-            var stat = props.stats[item.find('.review-item-label__text').text().trim()];
-            if (!stat) {
-                item.remove();
-            }
-            jQuery(this).find('.stars-result').css('width', stat + '%');
-            var score = Stats.getStatScore(stat, Stats.getProps());
-            item.find('.stars-result').width(stat + "%");
-            item.find('input').val(stat);
-            item.find('.review-item-stars').attr('result', stat);
-            item.find('.review-item-label__score').text(score);
-        });
+        // Non-logged-in Users
+        // form.find('[name="user_name"]').attr("disabled", "");
+        // form.find('[name="user_email"]').attr("disabled", "");
+        // form.find('[name="website"]').attr("disabled", "");
 
+        // Stats
+        Edit.getModifiedFormforStats(props, form);
         // ProsandCons 
         Edit.getModifiedFormforProsandCons(props, form, 'pros');
         Edit.getModifiedFormforProsandCons(props, form, 'cons');
+        // Attachments
 
         return form;
     },
 
+    getModifiedFormforStats: function (props, form) {
+        if (props.stats) {
+            form.find('.review-list li').each(function () {
+                var item = jQuery(this);
+                var stat = props.stats[item.find('.review-item-label__text').text().trim()];
+                if (!stat) {
+                    item.remove();
+                }
+                jQuery(this).find('.stars-result').css('width', stat + '%');
+                var score = Stats.getStatScore(stat, Stats.getProps());
+                item.find('.stars-result').width(stat + "%");
+                item.find('input').val(stat);
+                item.find('.review-item-stars').attr('result', stat);
+                item.find('.review-item-label__score').text(score);
+            });
+        }
+    },
+
     getModifiedFormforProsandCons: function (props, form, listname) {
-        if (props.prosandcons[listname + '-list'].length > 1) {
+        if (props.prosandcons && props.prosandcons[listname + '-list'].length > 1) {
             for (var ii = 1; ii < props.prosandcons[listname + '-list'].length; ii++) {
                 form.find('.review-' + listname + '-repeater [data-repeater-create]').trigger("click");
             }
@@ -108,7 +117,7 @@ var Edit = {
 
         var index = 0;
         form.find('.review-' + listname + '-repeater [data-repeater-item]').each(function () {
-            if (props.prosandcons[listname + '-list'][index]) {
+            if (props.prosandcons && props.prosandcons[listname + '-list'][index]) {
                 jQuery(this).find(".prosandcons").dropdownX("set text", props.prosandcons[listname + '-list'][index].item);
             } else {
                 jQuery(this).find("[data-repeater-delete]").trigger("click");
