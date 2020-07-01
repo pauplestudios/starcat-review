115fdbae45b3ac5794b3d91c8b3be41a08b24a22
Santhosh <santhoshoutright@gmail.com>
2020-07-01 15:43:48 +0530

    Part 1 done #SR-240: woocomerc product review plugin pro template replacing fix

--- features/woocommerce-integration.php
+++ features/woocommerce-integration.php
@@ -14,22 +14,24 @@ if (!class_exists('\StarcatReview\Features\Woocommerce_Integration')) {
         public function __construct()
         {
             /*
-             * Overriding the Existing product template by adding 11 as filter priotiry
+             * Overriding the Existing product template by adding 99 as filter priotiry
              */
-            add_filter('comments_template', [$this, 'comments_template_loader'], 11);
+            add_filter('comments_template', [$this, 'comments_template_loader'], 99);
             add_filter('scr_convert_product_rating_to_stat', [$this, 'convert_product_rating_to_stat']);
         }
 
         public function comments_template_loader($template)
         {
-            if (get_post_type() !== 'product') {
-                return $template;
-            }
+            $post_type = get_post_type();
+            $enabled_post_types = SCR_Getter::reviews_enabled_post_types();
 
             $dir = SCR_PATH . '/app/templates/';
-            if (file_exists(trailingslashit($dir) . 'reviews-template.php')) {
-                $template = trailingslashit($dir) . 'reviews-template.php';
+            if ($post_type == 'product' && in_array($post_type, $enabled_post_types)) {
+                if (file_exists(trailingslashit($dir) . 'reviews-template.php')) {
+                    $template = trailingslashit($dir) . 'reviews-template.php';
+                }
             }
+
             return $template;
         }
 
