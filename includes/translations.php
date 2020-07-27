26118e02fe130d74e33a9f0291ad0fb3ee8b4499
Santhosh <santhoshoutright@gmail.com>
2020-07-27 15:17:42 +0530

    Review fail message updated

--- includes/translations.php
+++ includes/translations.php
@@ -54,8 +54,8 @@ if (!class_exists('\StarcatReview\Includes\Translations')) {
                 'existscons' => __('Type new or select existing cons', SCR_DOMAIN),
                 'reviewSuccessTitle' => __('Thanks for your review', SCR_DOMAIN),
                 'reviewSuccessDescription' => __('You can see your review below. Also, look at the users summary', SCR_DOMAIN),
-                'reviewFailTitle' => __('This is a Bad request, Our development team processing it for while so we suggest you should keep browsing!', SCR_DOMAIN),
-                'reviewFailDescription' => __('Thanks for your review though', SCR_DOMAIN),
+                'reviewFailTitle' => __('Review failed. Please try again later!', SCR_DOMAIN),
+                'reviewFailDescription' => __('If it keeps failing, please contact the admin of the site', SCR_DOMAIN),
             ];
         }
     } // END CLASS
