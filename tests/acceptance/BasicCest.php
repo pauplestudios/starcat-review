c57e5bdccb827d3e24404ba16e4f07bfe43b5399
Santhosh <santhoshoutright@gmail.com>
2020-08-24 00:36:44 +0530

    Photo reviews addon and Woo Notification addon basic cest added and back to previous wp-browser version

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -104,4 +104,62 @@ class BasicCest
         /* Check Available Menus */
         $I->see('Comparison Table');
     }
+
+    public function woocommerceNotficationAddonActivation(\AcceptanceTester $I)
+    {
+        $data_slug = 'starcat-review-woocommerce-notfication-addon';
+        $I->loginAsAdmin();
+
+        // 1. When Parent plugin is deactivated
+        $I->amOnPluginsPage();
+        $I->deactivatePlugin('starcat-review');
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginDeactivated('starcat-review');
+        $I->activatePlugin($data_slug);
+        $I->seeElement('.error');
+
+        // 2. When Parent plugin is activated
+        $I->amOnPluginsPage();
+        $I->activatePlugin('starcat-review');
+        $I->activatePlugin($data_slug);
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginActivated($data_slug);
+
+        /* Settings Page */
+        $I->amOnPage('/wp-admin/admin.php?page=scr-settings#tab=1');
+
+        /* Check Available Menus */
+        $I->see('Woocommerce Notification');
+    }
+
+    public function photoReviewsAddonActivation(\AcceptanceTester $I)
+    {
+        $data_slug = 'starcat-review-photo-reviews-addon';
+        $I->loginAsAdmin();
+
+        // 1. When Parent plugin is deactivated
+        $I->amOnPluginsPage();
+        $I->deactivatePlugin('starcat-review');
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginDeactivated('starcat-review');
+        $I->activatePlugin($data_slug);
+        $I->seeElement('.error.src-error.missing-parent');
+
+        // 2. When Parent plugin is activated
+        $I->amOnPluginsPage();
+        $I->activatePlugin('starcat-review');
+        $I->activatePlugin($data_slug);
+        $I->amOnPagesPage();
+        $I->amOnPluginsPage();
+        $I->seePluginActivated($data_slug);
+
+        /* Settings Page */
+        $I->amOnPage('/wp-admin/admin.php?page=scr-settings#tab=1');
+
+        /* Check Available Menus */
+        $I->see('Photo Reviews');
+    }
 }
