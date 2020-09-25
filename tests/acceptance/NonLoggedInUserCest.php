f33ddda0e37f6b817bca2c3c0d171c6b9b3ccdab
essekia <essekiarockz@gmail.com>
2020-09-25 18:23:40 +0530

    Test updated

--- tests/acceptance/NonLoggedInUserCest.php
+++ tests/acceptance/NonLoggedInUserCest.php
@@ -23,6 +23,7 @@ class NonLoggedInUserCest
 
 
         $I->amOnPage('/product/album/');
+        $I->see('Reviews');
         $I->see('Reviews (0)');
         $I->click('#tab-reviews');
         $I->see('Leave a Review');
