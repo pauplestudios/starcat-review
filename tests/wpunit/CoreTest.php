a7a0cdf127fa36c4b0cbc9e5894ac9fd27d2148f
santhosh <santhoshoutright@gmail.com>
2020-12-04 12:20:31 +0530

    tests submodule repository path change to vendor folder

--- tests/wpunit/CoreTest.php
+++ tests/wpunit/CoreTest.php
@@ -36,7 +36,7 @@ class CoreTest extends \Codeception\TestCase\WPTestCase
     public function test_create_article()
     {
         $id = wp_insert_post(array('post_title' => 'Test Create Article', 'post_type' => 'pauple_helpie', 'post_content' => 'Test CPT text'));
-        $this->assertInternalType("int", $id);
+        $this->assertIsInt($id);
     }
 
     public function create_new_user()
