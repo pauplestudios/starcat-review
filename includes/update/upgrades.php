19570f72529acc6ed9c4d2b735c8c4f2c9702cc7
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2020-12-09 16:06:32 +0530

    add method to clearing current version cache

--- includes/update/upgrades.php
+++ includes/update/upgrades.php
@@ -20,10 +20,13 @@ if (!class_exists('\StarcatReview\Includes\Update\Upgrades')) {
         public static function init()
         {
             // error_log('Upgrades->init()');
+
+            self::clear_current_verion_cache();
+
             $args = [
-                'db_version' => get_option('SCR_VERSION'),
+                'db_version' => get_option('SCR_VERSION'), //starcat_review_option
                 'file_version' => SCR_VERSION,
-                'version_option' => 'SCR_VERSION',
+                'version_option' => 'SCR_VERSION', //starcat_review_option
                 'slug' => 'slug',
             ];
 
@@ -37,5 +40,37 @@ if (!class_exists('\StarcatReview\Includes\Update\Upgrades')) {
             }
 
         }
+
+        public static function clear_current_verion_cache(){
+            error_log('*** init clear_current_verion_cache ***');
+            $current_version = SCR_VERSION;
+            error_log('[$current_version] : ' .$current_version );
+            //check & update db version
+
+            if($current_version == get_option('SCR_VERSION')){
+                return;
+            }
+            
+            $upgrades = get_option('slug_upgrades');
+            error_log('upgrades1 : ' . print_r($upgrades, true));
+            if(!isset($upgrades[$current_version]) || empty($upgrades)) {
+                return;
+            }
+
+            $upgrades_versions = array_keys($upgrades);
+            error_log('[$upgrades_versions] : ' . print_r($upgrades_versions, true));
+            $last_version_index =  array_search($current_version,$upgrades_versions) - 1;
+            $last_db_version  = isset($upgrades_versions[$last_version_index]) ? $upgrades_versions[$last_version_index]: '';
+            error_log('[$last_db_version] : ' .$last_db_version );
+            unset($upgrades[$current_version]);
+            update_option('slug_upgrades',$upgrades);
+            if(!empty($last_db_version)){
+                update_option('SCR_VERSION',$last_db_version);
+            }
+
+            return true;
+
+        }
+
     } // END CLASS
 }
