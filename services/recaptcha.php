d0c77c88a1bbd75cc0292512c6a5640b88413df3
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2020-12-15 11:23:58 +0530

    Done #SR-321 captcha is working

--- services/recaptcha.php
+++ services/recaptcha.php
@@ -47,32 +47,21 @@ if (!class_exists('\StarcatReview\Services\Recaptcha')) {
             $post = get_post($post_id);
             $secret_key = SCR_Getter::get('recaptcha_secret_key');
             if(isset($post) && $post->post_type == 'product'){
-                error_log('*** WC3 ***');
                 $secret_key = SCR_Getter::get('woo_recaptcha_secret_key');
             }
-            error_log('[$secret_key] : ' .$secret_key );
             
-            error_log('Requests : ' . print_r($_REQUEST, true));
-            
-            $captcha = isset($_POST["captcha"]) &&  !empty($_POST["captcha"]) ? $_POST["captcha"] : '';
-            if(empty($captcha)){
+            $captcha_response = isset($_POST["captcha"]) &&  !empty($_POST["captcha"]) ? $_POST["captcha"] : '';
+            if(empty($captcha_response)){
                 return false;
             } 
             
-            $response = sanitize_key(wp_unslash($captcha));
-            // $response = sanitize_key($captcha);
-            // error_log('verify');
-            // error_log('verify');
-            error_log('$response : ' . print_r($response, true));
-
             $url = 'https://www.google.com/recaptcha/api/siteverify';
             $data = array(
                 'secret' => $secret_key,
-                'response' => $response,
+                'response' => $captcha_response,
             );
 
             $query = http_build_query($data);
-            error_log('[$query] : ' .$query );
             $options = array(
                 'http' => array(
                     'header' => "Content-Type: application/x-www-form-urlencoded\r\n" .
@@ -85,9 +74,7 @@ if (!class_exists('\StarcatReview\Services\Recaptcha')) {
 
             $context = stream_context_create($options);
             $verify = file_get_contents($url, false, $context);
-            error_log('[verify] : ' .$verify );
             $captcha_success = json_decode($verify);
-            error_log('captcha : ' . print_r($captcha_success, true));
             if ($captcha_success->success == false) {
                 // echo "<p>You are a bot! Go away!</p>";
                 $captcha_success = false;
