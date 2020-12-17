9a0ea515c42c4c7c323cae3961269e969e675307
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2020-12-17 13:21:47 +0530

    refactoring the file attachmentsvalidations + adding file-formats validation

--- includes/assets/js/components/user-review/form.js
+++ includes/assets/js/components/user-review/form.js
@@ -47,60 +47,60 @@ var Form = {
 
     submission: function (SCRForm, fields) {
         var form_data = Form.getProps(SCRForm, fields);
-        SCRForm.find(".submit.button").addClass("loading");
+        // SCRForm.find(".submit.button").addClass("loading");
         // Ajax Post Submiting        
-        jQuery.ajax({
-            type: "post",
-            url: scr_ajax.ajax_url,
-            cache: false,
-            data: form_data,
-            processData: false, // Preventing default data parse behavior                        
-            contentType: false,
-            success: function (results) {
+        // jQuery.ajax({
+        //     type: "post",
+        //     url: scr_ajax.ajax_url,
+        //     cache: false,
+        //     data: form_data,
+        //     processData: false, // Preventing default data parse behavior                        
+        //     contentType: false,
+        //     success: function (results) {
 
-                // Photo Reviews json response 
-                if(results.addon === 'SCR_PR'){
-                    if(results.status == 2){
-                        alert(results.message);    
-                        // Reloading the page
-                        window.location.reload();
-                        return false;
-                    }
-                }
-                results = JSON.parse(results);
+        //         // Photo Reviews json response 
+        //         if(results.addon === 'SCR_PR'){
+        //             if(results.status == 2){
+        //                 alert(results.message);    
+        //                 // Reloading the page
+        //                 window.location.reload();
+        //                 return false;
+        //             }
+        //         }
+        //         results = JSON.parse(results);
 
 
-                // Success Message
-                var msgProps = {
-                    type: "positive",
-                    title: Translations.reviewSuccessTitle,
-                    description: Translations.reviewSuccessDescription,
-                };
+        //         // Success Message
+        //         var msgProps = {
+        //             type: "positive",
+        //             title: Translations.reviewSuccessTitle,
+        //             description: Translations.reviewSuccessDescription,
+        //         };
 
-                SCRForm.html(Form.getMessageTemplate(msgProps));
+        //         SCRForm.html(Form.getMessageTemplate(msgProps));
 
-                // Reviewed item prepending to Reviews List
-                // jQuery("#scr-cat-collection").prepend(
-                //     Form.getReviewTemplate(props.title, props.description)
-                // );
+        //         // Reviewed item prepending to Reviews List
+        //         // jQuery("#scr-cat-collection").prepend(
+        //         //     Form.getReviewTemplate(props.title, props.description)
+        //         // );
                 
-                // Reloading the page
-                setInterval("window.location.reload()", 5000);
-            }
-        }).fail(function (response) {
-            console.log("!!! Submision Failed !!!");
-            console.log(response);
-            // Fail Message
-            var msgProps = {
-                type: "negative",
-                title: Translations.reviewFailTitle,
-                description: Translations.reviewFailDescription,
-            };
-            SCRForm.html(Form.getMessageTemplate(msgProps));
+        //         // Reloading the page
+        //         setInterval("window.location.reload()", 5000);
+        //     }
+        // }).fail(function (response) {
+        //     console.log("!!! Submision Failed !!!");
+        //     console.log(response);
+        //     // Fail Message
+        //     var msgProps = {
+        //         type: "negative",
+        //         title: Translations.reviewFailTitle,
+        //         description: Translations.reviewFailDescription,
+        //     };
+        //     SCRForm.html(Form.getMessageTemplate(msgProps));
 
-            // Reloading the page
-            setInterval("window.location.reload()", 5000);
-        }, JSON);
+        //     // Reloading the page
+        //     setInterval("window.location.reload()", 5000);
+        // }, JSON);
     },
 
     getProps: function (submittingForm, fields) {
@@ -261,22 +261,25 @@ var Form = {
     imageValidation: function (SCRForm){
         var filesCount = 0;
         var files = [];
+        var uploadField = document.getElementById('scr_pr_image_upload');
+        files = (uploadField) ? uploadField.files : [];
+        filesCount = files.length;
+
+        var uploadedImageGroup = SCRForm.find('.field .images.scr_pr_uploaded_image_group');
+        var uploadedImages = uploadedImageGroup.find('.deleteable.image').length;
+       
+        filesCount = parseInt(uploadedImages) + parseInt(filesCount);
+        
         if(SCROptions.required_options.pr_require_photo == 1){
-            var uploadField = document.getElementById('scr_pr_image_upload');
-            files = (uploadField) ? uploadField.files : [];
-            var uploadedImageGroup = SCRForm.find('.field .images.scr_pr_uploaded_image_group');
-            var uploadedImages = uploadedImageGroup.find('.deleteable.image').length;
-           
-            if(files.length == 0 && uploadedImages == 0){
+            if(filesCount == 0 && uploadedImages == 0){
                 return {
                     status: 2,
                     res_type: 'PHOTO_REQUIRED',
                     message: 'Photo is Required'
                 };
             }
-            filesCount = files.length;
         }
-       
+
         // Checking Files Quantities
         if(filesCount > SCROptions.required_options.pr_photo_quantity){
             return {
@@ -286,11 +289,14 @@ var Form = {
             };
         }
 
+        var allowedFileFormats = ['image/jpg','image/jpeg','image/bmp','image/png','image/gif'];
+
         // photo max file size checking
         var maxPhotoSize =  SCROptions.required_options.pr_photo_size;
-        for(ii = 0; ii < filesCount; ii++){
+        for(ii = 0; ii < files.length; ii++){
             var size = files[ii].size;
             var fileSize = Math.round((size / 1024));    
+            var fileType = files[ii].type;
             if(fileSize > maxPhotoSize){
                 return {
                     status: 2,
@@ -298,8 +304,15 @@ var Form = {
                     message: 'Max size allowed: '+maxPhotoSize+'kB'
                 };
             }
-        }
 
+            if(allowedFileFormats.indexOf(fileType) == -1){
+                return {
+                    status: 2,
+                    res_type: 'INVALID_FILE_FORMATS',
+                    message: 'Only JPG, JPEG, BMP, PNG and GIF are allowed'
+                };
+            }
+        }
 
         return {
             status: 1,
