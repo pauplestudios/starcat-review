a5d975e360001de0e40b3c48e5e2045b1c341941
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-03-03 18:24:59 +0530

    removed user-review.php file

--- app/builders/review-builder.php
+++ app/builders/review-builder.php
@@ -11,22 +11,24 @@ if (!defined('ABSPATH')) {
 if (!class_exists('\StarcatReview\App\Builders\Review_Builder')) {
     class Review_Builder
     {
-        public function __construct()
-        {
-            $this->user_review = new \StarcatReview\App\Widget_Makers\User_Review();
-        }
-
         public function get_reviews()
         {
             $html = '';
             $post_type = get_post_type();
             $enabled_post_types = SCR_Getter::get_review_enabled_post_types();
+            $ur_handler = new \StarcatReview\App\Widget_Makers\User_Review\Handler();
             $ur_summary = new \StarcatReview\App\Widget_Makers\User_Review\Summary();
+            $ur_lists = new \StarcatReview\App\Widget_Makers\User_Review\Lists();
+            $ur_form = new \StarcatReview\App\Widget_Makers\User_Review\Form();
+
+            $args = $ur_handler->get_default_args();
+
             $settings_args = $ur_summary->get_settings_args();
 
             if (in_array($post_type, $enabled_post_types)) {
                 $html .= $ur_summary->get_summary_view($settings_args);
-                $html .= $this->user_review->get_view();
+                $html .= $ur_form->get_form($args);
+                $html .= $ur_lists->get_lists_view($args);
             }
 
             return $html;
