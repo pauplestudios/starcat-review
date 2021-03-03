d176730249a986fe4f748c0ca3c34db23babe903
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-03-03 17:54:50 +0530

    shortcode attributes conditions added

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -81,13 +81,15 @@ if (!class_exists('\StarcatReview\Includes\Shortcodes')) {
 
         public function user_review($atts)
         {
-            // default user attributes
+            // 1. define as default user attributes
             $default_user_args = [
-                'show_pros_cons' => true,
-                'show_overall_only' => false,
                 'show_stats' => true,
+                'show_form' => true,
+                'show_lists' => true,
+                'show_summary' => true,
                 'post_id' => get_the_ID(),
             ];
+            // 2. merge user default attributes with shortcode atts
             $user_args = shortcode_atts($default_user_args, $atts);
 
             $user_review_handler = new \StarcatReview\App\Widget_Makers\User_Review\Handler();
@@ -95,11 +97,28 @@ if (!class_exists('\StarcatReview\Includes\Shortcodes')) {
             $form = new \StarcatReview\App\Widget_Makers\User_Review\Form();
             $lists = new \StarcatReview\App\Widget_Makers\User_Review\Lists();
 
+            //3. set empty value for all view component
+            $summary_view = $form_view = $review_list_view = '';
+
+            // 4. get default args
             $args = $user_review_handler->get_default_args($user_args);
-            $summary_args = $summary->get_settings_args($args);
-            $form_view = $form->get_form($args);
-            $summary_view = $summary->get_summary_view($summary_args);
-            $review_list_view = $lists->get_lists_view($args);
+
+            // 5. shown summary, if the user args have show_summary attributes is true. Otherwise doesn't shown summary.
+            if ($user_args['show_summary'] === true) {
+                $summary_args = $summary->get_settings_args($args);
+                $summary_view = $summary->get_summary_view($summary_args);
+            }
+
+            // 6. shown review form, if the user args have show_form attributes is true. Otherwise doesn't shown the review form.
+            if ($user_args['show_form'] === true) {
+                error_log('*** Its Not Working ***');
+                $form_view = $form->get_form($args);
+            }
+
+            // 7. shown review lists, if the user args have show_lists attributes is true. Otherwise doesn't shown the review lists.
+            if ($user_args['show_lists'] === true) {
+                $review_list_view = $lists->get_lists_view($args);
+            }
 
             return $summary_view . $form_view . $review_list_view;
         }
