ed318a24e99bd0609a4dd174efbb644e30564f27
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-03-05 19:20:22 +0530

    write pending shortcode testcases

--- tests/acceptance/ShortcodesCest.php
+++ tests/acceptance/ShortcodesCest.php
@@ -35,7 +35,6 @@ class ShortcodeCest
         $I->seeElement('.prosandcons');
         $I->seeElement('.dropdown.sort');
 
-
         // 4. Check by changing different props
         $content = '[starcat_review_overall_user_review post_id=' . $review_post_id . ' show_review_search=0 show_review_sort=0]';
         $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
@@ -70,8 +69,7 @@ class ShortcodeCest
                 ),
             ),
             'comment_id' => '2',
-            'votes' =>
-            array(),
+            'votes' => array(),
         );
 
         $comment_id = $I->haveCommentInDatabase($review_post_id, ['comment_content' => $reviewProp['description'], 'comment_type' => 'review']);
@@ -79,26 +77,90 @@ class ShortcodeCest
         $I->haveCommentMetaInDatabase($comment_id, 'scr_user_review_props', $reviewProp);
     }
 
-    // public function test_user_review_list_shortcode(AcceptanceTester $I)
-    // {
-    //     $post_name = 'user-review-list';
-    //     $content = '[starcat_review_user_review_list]';
-    //     $shortcode_post_id = $this->insert_shortcode_post($I, $content, $post_name);
-    // }
+    public function userReviewListShortcodeCest(AcceptanceTester $I)
+    {
+        $shortcode_post_name = 'user-review-list';
+        $review_post_id = $this->insert_post($I);
+        $content = '[starcat_review_user_review_list post_id=' . $review_post_id . ' show_review_search="1" show_review_title="1" show_review_sort="1"]';
+        $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
+        codecept_debug(' [shortcode_post_id] ' . $shortcode_post_id);
 
-    // public function test_user_review_form_shortcode(AcceptanceTester $I)
-    // {
-    //     $post_name = 'user-review-form';
-    //     $content = '[starcat_review_user_review_form]';
-    //     $shortcode_post_id = $this->insert_shortcode_post($I, $content, $post_name);
-    // }
+        // 1. Insert single User Review
+        $this->insert_user_review($I, $review_post_id);
+        $I->amOnPage('/car-reviews');
+        $I->see('This Car is too expensive');
 
-    // public function test_review_summary_shortcode(AcceptanceTester $I)
-    // {
-    //     $post_name = 'user-review-summary';
-    //     $content = '[starcat_review_summary]';
-    //     $shortcode_post_id = $this->insert_shortcode_post($I, $content, $post_name);
-    // }
+        // 2. Check Shortcode Page
+        $I->amOnPage('/' . $shortcode_post_name);
+        $I->see('This Car is too expensive');
+
+        // 3. Check Major Components with Default Props
+        $I->seeElement('.scr-search');
+        $I->seeElement('.dropdown.sort');
+        $I->seeElement('h3.header');
+
+        $content = '[starcat_review_user_review_list post_id=' . $review_post_id . ' show_review_search="1" show_review_title="1" show_review_sort="1"]';
+        $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
+        codecept_debug('[$shortcode_post_id] 1 : ' . $shortcode_post_id);
+
+        $I->amOnPage('/' . $shortcode_post_name);
+        $I->dontSeeElement('.scr-search');
+        $I->dontSeeElement('.dropdown.sort');
+    }
+
+    public function userReviewFormCest(AcceptanceTester $I)
+    {
+        $shortcode_post_name = 'user-review-form';
+        $review_post_id = $this->insert_post($I);
+        $content = '[starcat_review_user_review_form post_id=' . $review_post_id . ']';
+        $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
+
+        // 1. Insert single User Review
+        $this->insert_user_review($I, $review_post_id);
+        $I->amOnPage('/car-reviews');
+        $I->see('This Car is too expensive');
+
+        // 2. Check Shortcode Page
+        $I->amOnPage('/' . $shortcode_post_name);
+        $I->see('This Car is too expensive');
+
+        // 3.  Check Major Components with Default Props
+        $review_form_class = '.scr-user-review-form-' . $review_post_id;
+        $I->seeElement($review_form_class);
+
+    }
+
+    public function userReviewSummary(AcceptanceTester $I)
+    {
+        $shortcode_post_name = 'user-review-summary';
+        $review_post_id = $this->insert_post($I);
+        $content = '[starcat_review_summary post_id=' . $review_post_id . ' show_author_reviews_summary="1" show_user_reviews_summary="1" show_pros_and_cons_summary="1"]';
+        $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
+
+        // 1. Insert single User Review
+        $this->insert_user_review($I, $review_post_id);
+        $I->amOnPage('/car-reviews');
+        $I->see('This Car is too expensive');
+
+        // 2. Check Shortcode Page
+        $I->amOnPage('/' . $shortcode_post_name);
+        // $I->see('This Car is too expensive');
+
+        // 3. Check Major Components with Default Props
+
+        $I->seeElement('.scr-summary'); // Checking Summary root element
+        $I->seeElement('.reviewed-list'); // checking reviewed-list items element
+        // $I->seeElement('.prosandcons'); // TODO need to checking author prosandcons element, not to added
+
+        // 4. Check by changing different props
+        $content = '[starcat_review_summary post_id=' . $review_post_id . ' show_author_reviews_summary="0" show_user_reviews_summary="0" show_pros_and_cons_summary="0"]';
+        $shortcode_post_id = $this->insert_shortcode_post($I, $content, $shortcode_post_name);
+
+        $I->amOnPage('/' . $shortcode_post_name);
+
+        $I->dontSeeElement('.scr-summary .reviewed-list');
+        $I->dontSeeElement('.prosandcons');
+    }
 
     protected function insert_shortcode_post($I, $post_content, $post_name)
     {
@@ -108,7 +170,7 @@ class ShortcodeCest
                 'post_name' => $post_name,
                 'post_type' => 'page',
                 'post_title' => 'Starcat Reviews - Overall User Reviews Shortcode',
-                'post_content' =>  $post_content,
+                'post_content' => $post_content,
                 'post_status' => 'publish',
             ]
         );
@@ -121,6 +183,21 @@ class ShortcodeCest
 
         $post_id = $I->havePostInDatabase([
             'post_type' => 'post',
+            'post_title' => 'Car Reviews',
+            'post_name' => 'car-reviews',
+            'post_status' => 'publish',
+            'post_content' => 'This car is great!',
+
+        ]);
+
+        return $post_id;
+    }
+
+    protected function insert_page($I)
+    {
+
+        $post_id = $I->havePostInDatabase([
+            'post_type' => 'page',
             'post_title' => 'Car Reviews',
             'post_name' => 'car-reviews',
             'post_status' => 'publish',
