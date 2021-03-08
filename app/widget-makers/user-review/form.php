45c39f4f67a7fbdba7a123c314ae4f9c03dafc83
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-03-08 08:53:30 +0530

    load only single form, when the page load

--- app/widget-makers/user-review/form.php
+++ app/widget-makers/user-review/form.php
@@ -9,10 +9,31 @@ if (!defined('ABSPATH')) {
 if (!class_exists('\StarcatReview\App\Widget_Makers\User_Review\Form')) {
     class Form
     {
+        public $limit = 0;
+
         public function get_form(array $args, array $user_args = array())
         {
+            $this->set_limit();
+            $limit = $this->get_limit();
+            if ($limit > 1) {
+                return;
+            }
             $form_controller = new \StarcatReview\App\Components\Form\Controller();
             return $form_controller->get_view($args, $user_args);
         }
+
+        public function set_limit()
+        {
+            global $SCR_FORM_LIMIT;
+            $this->limit = $SCR_FORM_LIMIT + 1;
+            $SCR_FORM_LIMIT = $this->limit;
+        }
+
+        public function get_limit()
+        {
+            global $SCR_FORM_LIMIT;
+            return empty($SCR_FORM_LIMIT) ? 1 : $SCR_FORM_LIMIT;
+        }
+
     }
 }
