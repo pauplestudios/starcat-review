c13cde5aa61482ea244d3b28137fc958f996fa09
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-04-07 16:43:54 +0530

    conditions + comments are added for show the author reviews in single post

--- app/capabilities/author-reviews.php
+++ app/capabilities/author-reviews.php
@@ -12,10 +12,22 @@ if (!class_exists('\StarcatReview\App\Capabilities\Author_Reviews')) {
         public function can_show_author_review(array $args = array())
         {
             $post_type = get_post_type();
+            // Get author reviews capabilities
             $author_review_caps = isset($args['post_author_review_caps']) ? $args['post_author_review_caps'] : [];
+
+            // check the author review as custom location or not.
+            $custom_author_review_location = isset($author_review_caps['enable_ar_custom_location']) && $author_review_caps['enable_ar_custom_location'] == 1 ? true : false;
+
+            // don't show the author reviews if users choose the "ar_post_location" option value as shortcode.
+            $use_shorcode = (isset($author_review_caps['ar_post_location']) && $custom_author_review_location && $author_review_caps['ar_post_location'] == 'shortcode') ? true : false;
+
+            // show the author reviews or not for current post
             $can_show_ar_in_post = isset($author_review_caps) && $author_review_caps['can_show_ar_in_post'] != 'dont_show' ? true : false;
+
             $ar_enabled_post_types = isset($args['ar_enabled_post_types']) ? $args['ar_enabled_post_types'] : [];
-            $can_show = (in_array($post_type, $ar_enabled_post_types) && $can_show_ar_in_post) ? true : false;
+
+            $can_show = (in_array($post_type, $ar_enabled_post_types) && !$use_shorcode && $can_show_ar_in_post) ? true : false;
+
             return $can_show;
         }
     }
