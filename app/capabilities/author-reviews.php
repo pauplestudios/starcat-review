8ce5545eb608bd18853bed55602f458d0caf8d01
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-04-08 14:41:58 +0530

    array index changes

--- app/capabilities/author-reviews.php
+++ app/capabilities/author-reviews.php
@@ -16,17 +16,17 @@ if (!class_exists('\StarcatReview\App\Capabilities\Author_Reviews')) {
             $author_review_caps = isset($args['post_author_review_caps']) ? $args['post_author_review_caps'] : [];
 
             // check the author review as custom location or not.
-            $custom_author_review_location = isset($author_review_caps['enable_ar_custom_location']) && $author_review_caps['enable_ar_custom_location'] == 1 ? true : false;
+            $custom_location = isset($author_review_caps['custom_location']) && $author_review_caps['custom_location'] == 1 ? true : false;
 
-            // don't show the author reviews if users choose the "ar_post_location" option value as shortcode.
-            $use_shorcode = (isset($author_review_caps['ar_post_location']) && $custom_author_review_location && $author_review_caps['ar_post_location'] == 'shortcode') ? true : false;
+            // don't show the author reviews if users choose the "location" option value as shortcode.
+            $use_shortcode = (isset($author_review_caps['location']) && $custom_location && $author_review_caps['location'] == 'shortcode') ? true : false;
 
             // show the author reviews or not for current post
-            $can_show_ar_in_post = isset($author_review_caps) && $author_review_caps['can_show_ar_in_post'] != 'dont_show' ? true : false;
+            $can_show_ar = isset($author_review_caps) && $author_review_caps['can_show_ar'] != 'dont_show' ? true : false;
 
             $ar_enabled_post_types = isset($args['ar_enabled_post_types']) ? $args['ar_enabled_post_types'] : [];
 
-            $can_show = (in_array($post_type, $ar_enabled_post_types) && !$use_shorcode && $can_show_ar_in_post) ? true : false;
+            $can_show = (in_array($post_type, $ar_enabled_post_types) && !$use_shortcode && $can_show_ar) ? true : false;
 
             return $can_show;
         }
