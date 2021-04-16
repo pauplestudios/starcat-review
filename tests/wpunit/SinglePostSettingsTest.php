ea57d150d95236b5375f556aedb54a7582fc40bd
theMechanicSekar <gnanasekaran.srgm@gmail.com>
2021-04-16 23:26:19 +0530

    Done, testcases passed

--- tests/wpunit/SinglePostSettingsTest.php
+++ tests/wpunit/SinglePostSettingsTest.php
@@ -21,8 +21,6 @@ class SinglePostTest extends \Codeception\TestCase\WPTestCase
     {
         $post_id = $this->create_post();
 
-        // $this->load_default_settings();
-
         $post_level_settings = new \StarcatReview\App\Post_Settings\Post_Level_Settings();
 
         $patterns = $this->get_patterns();
@@ -36,27 +34,28 @@ class SinglePostTest extends \Codeception\TestCase\WPTestCase
             $summary_args = $this->convert_to_integer($summary_args);
 
             $expected_data = $this->get_expected_data($pattern);
+
             codecept_debug(' [ ========= ' . $pattern . '=============]');
             codecept_debug('[$summary_args] : ' . print_r($summary_args, true));
             codecept_debug('[$expected_data] : ' . print_r($expected_data, true));
-            $this->assertEquals($summary_args, $expected_data);
 
+            $this->assertEquals($summary_args, $expected_data);
         }
     }
 
-    public function convert_to_integer()
+    public function convert_to_integer($summary_args)
     {
+        $args = array(
+            'after' => array(),
+            'before' => array(),
+        );
 
-    }
-
-    public function load_default_settings()
-    {
-        $option_name = 'scr_options';
-        $options = get_option($option_name);
-
-        codecept_debug(' [ ========= Options =============]');
-        codecept_debug($options);
-
+        foreach ($summary_args as $key => $summary) {
+            foreach ($summary as $setting_key => $setting_value) {
+                $args[$key][$setting_key] = ($setting_value == true) ? 1 : 0;
+            }
+        }
+        return $args;
     }
 
     public function get_patterns()
@@ -64,10 +63,10 @@ class SinglePostTest extends \Codeception\TestCase\WPTestCase
         return [
             'apply_global_settings',
             'dont_show',
-            // 'show_author_reviews_only',
-            // 'show_user_reviews_only',
-            // 'show_author_review_before_the_content',
-            // 'show_user_review_before_the_content',
+            'show_author_reviews_only',
+            'show_user_reviews_only',
+            'show_author_review_before_the_content',
+            'show_user_review_before_the_content',
         ];
     }
 
@@ -76,60 +75,60 @@ class SinglePostTest extends \Codeception\TestCase\WPTestCase
         $defaults = array(
             'apply_global_settings' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'apply_global_settings',
+                    'can_show_author_review' => 'apply_global_settings',
                     'custom_location' => 0,
                     'location' => 'after',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'apply_global_settings',
+                    'can_show_user_review' => 'apply_global_settings',
                     'custom_location' => 0,
                     'location' => 'after',
                 ),
             ),
             'dont_show' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'dont_show',
+                    'can_show_author_review' => 'dont_show',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'dont_show',
+                    'can_show_user_review' => 'dont_show',
                 ),
             ),
             'show_author_reviews_only' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'show',
+                    'can_show_author_review' => 'show',
                     'custom_location' => 0,
                     'location' => 'after',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'dont_show',
+                    'can_show_user_review' => 'dont_show',
                 ),
             ),
             'show_user_reviews_only' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'dont_show',
+                    'can_show_author_review' => 'dont_show',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'show',
+                    'can_show_user_review' => 'show',
                     'custom_location' => 0,
                     'location' => 'after',
                 ),
             ),
             'show_author_review_before_the_content' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'show',
+                    'can_show_author_review' => 'show',
                     'custom_location' => 1,
                     'location' => 'before',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'dont_show',
+                    'can_show_user_review' => 'dont_show',
                 ),
             ),
             'show_user_review_before_the_content' => array(
                 'post_author_review_settings' => array(
-                    'can_show_the_author_review' => 'dont_show',
+                    'can_show_author_review' => 'dont_show',
                 ),
                 'post_user_review_settings' => array(
-                    'can_show_the_user_review' => 'show',
+                    'can_show_user_review' => 'show',
                     'custom_location' => 1,
                     'location' => 'before',
                 ),
@@ -204,7 +203,7 @@ class SinglePostTest extends \Codeception\TestCase\WPTestCase
                 'after' => array(
                     'enable-author-review' => 0,
                     'enable_pros_cons' => 0,
-                    'enable_user_reviews' => 0,
+                    'enable_user_reviews' => 1,
                     'enable_atthachments' => 1,
                 ),
             ),
